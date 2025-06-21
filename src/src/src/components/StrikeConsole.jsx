f80475b0e949a2cf88f1684b1231f29464df91e3
Jerren Shaffer <88438765+Texasgold88@users.noreply.github.com>
2025-06-21 00:18:28 -0500

    Update StrikeConsole.jsx

--- src/src/src/components/StrikeConsole.jsx
+++ src/src/src/components/StrikeConsole.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { strikeLogs } from '../data/mockCreeps';
 
+const StrikeConsole = () => {
+  return (
+    <div className="panel">
+      <h2>🎯 Strike Console</h2>
+      <ul>
+        {strikeLogs.map((log, index) => (
+          <li key={index}>{log.msg}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default StrikeConsole;
