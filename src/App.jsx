c65b54a3f3f1f4f70f9300c874a5d0604a6d59b9
Jerren Shaffer <88438765+Texasgold88@users.noreply.github.com>
2025-06-21 00:11:50 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
 
+import React from 'react';
+import { baitFeed } from '../data/mockCreeps';
+
+const BaitFeed = () => {
+  return (
+    <div className="panel">
+      <h2>🎣 Bait Status Feed</h2>
+      <ul>
+        {baitFeed.map((bait) => (
+          <li key={bait.id}>
+            🪤 Decoy #{bait.id} | {bait.tokenPair} | {bait.bot} | Burned: {bait.burned}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BaitFeed;
