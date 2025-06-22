26b306ae24b5697aa634f167fec518fc3787d94c
Jerren Shaffer <88438765+Texasgold88@users.noreply.github.com>
2025-06-22 02:29:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
 
+import React from 'react'
+import mockCreeps from './data/mockCreeps'
+
+const App = () => {
+  return (
+    <div style={{ padding: "2rem", color: "#fff", background: "#121212" }}>
+      <h1>JusticeRadar: Creep Watch</h1>
+      <ul>
+        {mockCreeps.map(creep => (
+          <li key={creep.id}>
+            <strong>{creep.name}</strong> ({creep.wallet}) – {creep.reason}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
