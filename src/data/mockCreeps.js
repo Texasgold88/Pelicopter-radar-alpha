6f1584463ef5dcd71987e5e0c7f04574733ebfe7
Jerren Shaffer <88438765+Texasgold88@users.noreply.github.com>
2025-06-22 02:31:18 -0500

    Update mockCreeps.js

--- src/data/mockCreeps.js
+++ src/data/mockCreeps.js
@@ -1,1 +1,19 @@
 
+const mockCreeps = [
+  {
+    id: 1,
+    name: "Scammy McScammer",
+    wallet: "0x123...abc",
+    reason: "Rug pulled a DeFi token",
+    date: "2025-06-01"
+  },
+  {
+    id: 2,
+    name: "FakeDrop Fred",
+    wallet: "0x456...def",
+    reason: "Sent phishing airdrops",
+    date: "2025-06-02"
+  }
+];
+
+export default mockCreeps;
